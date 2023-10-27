8f601e125832afd4d7e3275f44bc85890e67d26f
Danil-27 <pde200126@gmail.com>
2023-10-27 12:56:07 +0300

    upDate

---
+++ main.js
@@ -0,0 +1,16 @@
+class ar {
+    static shohi(){
+        console.log('2131')
+    }
+}
+
+
+class ar1 extends ar {
+
+}
+
+// console.log(ar1.shohi())
+
+let object = new ar1()
+
+console.log(object.shohi())
